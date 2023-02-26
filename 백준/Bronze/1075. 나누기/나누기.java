76198b74c13ff952e8d0a2c822e43c6a40a087e4
Devyan0 <70054592+Devyan0@users.noreply.github.com>
2023-02-26 16:02:23 +0900

    [Bronze II] Title: 나누기, Time: 224 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/1075. 나누기/나누기.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int F = sc.nextInt();
+		N = N / 100;
+		for (int i=0; i<100; ++i) {
+			if ((N*100+i) % F == 0) {
+				System.out.printf("%02d", i);
+				break;
+			}
+		}
+	}
+}
