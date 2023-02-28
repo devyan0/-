907368a4bb41d1b86bbcc4dddd4f3483e62e52c4
Devyan0 <70054592+Devyan0@users.noreply.github.com>
2023-02-28 16:24:06 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 140 ms, Memory: 116104 KB -BaekjoonHub

---
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+s = input().strip()
+res = ''
+
+l = r = 0
+while r < len(s):
+    if s[l] == '<':
+        if s[r] == '>':
+            res += s[l:r+1]
+            l = r = r+1
+        else:
+            r += 1
+    elif s[r] == '<':
+        res += s[l:r][::-1]
+        l = r
+    elif s[r] == ' ':
+        res += s[l:r][::-1] + ' '
+        l = r = r + 1
+    else:
+        r += 1
+
+# switch l .. r
+res += s[l:r][::-1]
+print(res)
