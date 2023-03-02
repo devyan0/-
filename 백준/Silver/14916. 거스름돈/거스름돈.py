095f87fcc264a41909363a09e0b27b9358f323a2
Devyan0 <70054592+Devyan0@users.noreply.github.com>
2023-03-02 09:11:27 +0900

    [Silver V] Title: 거스름돈, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/14916. 거스름돈/거스름돈.py
+++ 백준/Silver/14916. 거스름돈/거스름돈.py
@@ -1,20 +1,13 @@
-n = int(input())
+remain = int(input())
 cnt = 0
-while True:
-    if n < 0:       # base case 1: impossible
-        cnt = -1
-        break
-    if n == 0:      # base case 2: complete
-        break
 
-    if n % 5 == 0:  # step 1: if n can be composed only with 5s
-        cnt += n // 5
-        n -= 5 * (n // 5)
-
-    if n == 0:      # step 1 success
+while 0 <= remain:
+    if remain % 5 == 0:
+        cnt += remain//5
+        remain = 0
         break
 
-    n -= 2          # step 2: use one coin of 2
+    remain -= 2
     cnt += 1
 
-print(cnt)
+print(cnt if remain == 0 else -1)
