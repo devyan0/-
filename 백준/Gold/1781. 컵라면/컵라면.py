551cdd2c400df2f03dd5ae5e43f4424958ff0f49
Devyan0 <70054592+Devyan0@users.noreply.github.com>
2023-03-02 10:03:00 +0900

    [Gold II] Title: 컵라면, Time: 672 ms, Memory: 151680 KB -BaekjoonHub

--- 백준/Gold/1781. 컵라면/컵라면.py
+++ 백준/Gold/1781. 컵라면/컵라면.py
@@ -1,20 +1,18 @@
-from heapq import heappush as push, heappop as pop
-import sys
-input = sys.stdin.readline
+'''
+주어진 정보들 중에서 선택하지 않을 정보를 제거하는 방식
+'''
+import heapq
+n = int(input())
+info = [list(map(int, input().split())) for _ in range(n)]
 
-N = int(input())
-info = sorted([list(map(int, input().split())) for _ in range(N)])
+info.sort(key = lambda x : x[0]) #데드라인 순으로 정렬함
+heap = []
+heapq.heapify(heap)
+for item in info:
+    deadline, ramen = item
+    heapq.heappush(heap, ramen) #라면의 개수를 힙에 넣어준다 (라면의 개수가 작은 것 순서로 정렬됨)
 
-res, save, t = 0, 1, info[-1][0]
-tasks = []
-while 0 < t:
-    while info and t <= info[-1][0]:
-        push(tasks, -info.pop()[1])
+    if len(heap) > deadline: #힙에 넣은 라면의 수가 데드라인을 넘어가면
+        heapq.heappop(heap) #제일 작은 라면의 개수 제거
 
-    if 0 < save and tasks:
-        res += -pop(tasks)
-        save -= 1
-
-    t, save = t-1, save+1
-
-print(res)
+print(sum(heap))
