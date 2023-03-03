5c7fabe49a7cfc2dbb6df732da7a73b6c3cbc978
Devyan0 <70054592+Devyan0@users.noreply.github.com>
2023-03-03 13:11:06 +0900

    [unrated] Title: 연속 펄스 부분 수열의 합, Time: 276.04 ms, Memory: 55.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.py
@@ -0,0 +1,17 @@
+def solution(sequence):
+    seq1 = [x if i%2 else -x for i, x in enumerate(sequence)]
+    seq2 = [-x if i%2 else x for i, x in enumerate(sequence)]
+    # print(seq1)
+    # print(seq2)
+    def kadane(seq):
+        max_ = 0
+        acc = 0
+        for n in seq:
+            if 0 < acc + n:
+                acc += n
+            else:
+                acc = 0
+            max_ = max(max_, acc)
+        return max_
+    
+    return max(map(kadane, [seq1, seq2]))
